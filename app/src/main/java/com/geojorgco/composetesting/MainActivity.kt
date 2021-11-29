71e70ba1c29aad15ae5de368f47d6d26a810eb56
Jorge Guerrero <54997758+geojorg@users.noreply.github.com>
2021-11-29 18:04:00 -0500

    Animation

--- app/src/main/java/com/geojorgco/composetesting/MainActivity.kt
+++ app/src/main/java/com/geojorgco/composetesting/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.geojorgco.composetesting
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import com.geojorgco.composetesting.topbar.TopBar
 import com.geojorgco.composetesting.ui.theme.ComposeTestingTheme
 
@@ -34,16 +35,29 @@ class MainActivity : ComponentActivity() {
 fun MenuDrawer(){
 
     val isShowing = remember { mutableStateOf(false) }
+    val geo = LocalDensity.current.density
+
+
+
+    val algoAnimation by animateIntOffsetAsState(
+        targetValue = if (isShowing.value)
+            IntOffset(350*geo.toInt(), 44) else IntOffset(0, 0),
+        animationSpec = tween(
+            durationMillis = 200, // duration
+            easing = LinearEasing
+        ),
+    )
+
+    val menuAnimation by animateFloatAsState(
+        targetValue = if (isShowing.value) 0.8f else 1f,
+        animationSpec = tween(durationMillis = 200,easing = LinearEasing)
+    )
 
     Scaffold(
-        modifier = Modifier
-            .animateContentSize(animationSpec = tween(durationMillis = 1000,
-                easing = LinearOutSlowInEasing)
-            ),
-                topBar = { TopBar(isShowing) }
+        topBar = { TopBar(isShowing) }
     ) {
         Menu()
-        ViewS(isShowing)
+        ViewS(isShowing, algoAnimation, menuAnimation)
     }
 }
 
--- app/src/main/java/com/geojorgco/composetesting/ViewS.kt
+++ app/src/main/java/com/geojorgco/composetesting/ViewS.kt
@@ -1,8 +1,5 @@
 package com.geojorgco.composetesting
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
@@ -14,25 +11,24 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ViewS(isShowing: MutableState<Boolean>){
+fun ViewS(isShowing: MutableState<Boolean>, algoAnimation: IntOffset, menuAnimation: Float){
     Column(
         modifier = Modifier
             .fillMaxHeight()
             .fillMaxWidth()
-            .scale(if (isShowing.value) 0.8f else 1f)
-            .offset(
-                x = if (isShowing.value) 300.dp else 0.dp,
-                y = if (isShowing.value) 44.dp else 0.dp
-            )
+            .scale(menuAnimation)
+            .offset {
+                algoAnimation
+            }
             .clip(if (isShowing.value) RoundedCornerShape(20.dp) else RoundedCornerShape(0.dp))
             .background(Color.LightGray)
     ) {
         Text(
-            modifier = Modifier.fillMaxWidth()
-                    ,
+            modifier = Modifier.fillMaxWidth(),
             text = "Hola")
     }
 }
