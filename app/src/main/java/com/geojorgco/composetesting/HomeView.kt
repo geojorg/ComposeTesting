cc7e775948bd249cbec15d8255a98c373ba0516d
Jorge Guerrero <54997758+geojorg@users.noreply.github.com>
2022-01-03 18:35:42 -0500

    Now Working

--- app/src/main/java/com/geojorgco/composetesting/HomeView.kt
+++ app/src/main/java/com/geojorgco/composetesting/HomeView.kt
@@ -1,5 +1,8 @@
 package com.geojorgco.composetesting
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -7,16 +10,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -25,6 +26,16 @@ import androidx.compose.ui.unit.dp
 fun HomeView(isShowing: MutableState<Boolean>, algoAnimation: IntOffset, menuAnimation: Float){
 
     val flipped = remember { mutableStateOf(false)}
+    val angle = 180F
+
+    val xRotationAngle by animateFloatAsState(
+        targetValue = if (flipped.value) 180F else 0F,
+        animationSpec = tween(durationMillis = 1100,easing = LinearEasing)
+    )
+    val yRotationAngle by animateFloatAsState(
+        targetValue = if (flipped.value) 180F else 0F,
+        animationSpec = tween(durationMillis = 1100,easing = LinearEasing)
+    )
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -37,12 +48,21 @@ fun HomeView(isShowing: MutableState<Boolean>, algoAnimation: IntOffset, menuAni
             .background(Color.LightGray)
     ) {
 
-        Image(painter = painterResource(id = R.drawable.card), contentDescription = "Card")
+        Image(
+            painter = painterResource(id = R.drawable.card),
+            contentDescription = "Card",
+            modifier = Modifier
+                .graphicsLayer {
+                    rotationY = yRotationAngle
+                    rotationX = xRotationAngle
+                    cameraDistance = 12f * density
+                }
+        )
         Text(
             modifier = Modifier.fillMaxWidth(),
             text = "Card of Clow")
         Spacer(modifier = Modifier.padding(bottom = 200.dp))
-        Button(onClick = {}) {
+        Button(onClick = {flipped.value = !flipped.value}) {
             Text("Flip Card")
         }
     }
